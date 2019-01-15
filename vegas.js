9359dcdde74de1afa82575fad3bee16ddc6c705c
triplelog <42818013+triplelog@users.noreply.github.com>
2019-01-15 11:36:05 -0500

    Update vegas.js

--- vegas.js
+++ vegas.js
@@ -1,5 +1,5 @@
 return function f(awin,aloss,hwin,hloss) {
-  var probAA = (awin+10)/(awin+aloss+10);
-  var probHH = (hwin+10)/(hwin+hloss+10);
+  var probAA = (awin+10)/(awin+aloss+20);
+  var probHH = (hwin+10)/(hwin+hloss+20);
   return (probHH-probAA*probHH)/(probAA+probHH-2*probAA*probHH);
 };
