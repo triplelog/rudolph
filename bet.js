e5e620e12156e01e15d33b0f035ed3387a362d8a
triplelog <42818013+triplelog@users.noreply.github.com>
2019-01-15 11:10:57 -0500

    Update bet.js

--- bet.js
+++ bet.js
@@ -1,5 +1,5 @@
 return function f(ars,ara,hrs,hra) {
   var probA = (Math.pow(ars+10,1.8))/(Math.pow(ars+10,1.8)+Math.pow(ara+10,1.8));
   var probH = (Math.pow(hrs+10,1.8))/(Math.pow(hrs+10,1.8)+Math.pow(hra+10,1.8));
-  return (probA-probA*probH)/(probA+probH-2*probA*probH);
+  return (probH-probA*probH)/(probA+probH-2*probA*probH);
 };
